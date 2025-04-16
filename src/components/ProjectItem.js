92605511ceff8f26a2948c5c192656f2b57731b1
Kelvin Mwirigi <kmwirigi05@gmail.com>
2025-04-16 09:23:18 +0300

    Adds files

--- src/components/ProjectItem.js
+++ src/components/ProjectItem.js
@@ -1,13 +1,12 @@
 import React from "react";
 
-function ProjectItem({ name, about, technologies }) {
+function ProjectItem({ name, technologies }) {
   return (
-    <div className="project-item">
-      <h3>{name}</h3>
-      <p>{about}</p>
-      <div className="technologies">
-        {/* render a <span> for each technology in the technologies array */}
-      </div>
+    <div>
+      <h3>{name}</h3> {/* Ensure name is displayed */}
+      {technologies.map((tech, index) => (
+        <span key={index}>{tech}</span>
+      ))}
     </div>
   );
 }
